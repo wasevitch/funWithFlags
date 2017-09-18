aa1f2a60b5b22bdc8e30023da1d262caa0ea758b
unknown <beauvoir.nicolas@gmail.com>
2017-09-18 14:18:38 +0200

    Delete garbages

--- MyApplication/app/src/main/java/com/wasevitch/ouzin_kane/funwithflags/modele/Drapeau.java
+++
@@ -1,8 +0,0 @@
-package com.wasevitch.ouzin_kane.funwithflags.modele;
-
-/**
- * Created by Jul on 18/09/2017.
- */
-
-public class Drapeau {
-}
--- MyApplication/app/src/main/java/com/wasevitch/ouzin_kane/funwithflags/test.java
+++
@@ -1,8 +0,0 @@
-package com.wasevitch.ouzin_kane.funwithflags;
-
-/**
- * Created by Jul on 18/09/2017.
- */
-
-public class test {
-}
